276c90c4342ffc322654542c79e2099852ba54d5
Adelin.Chis <Email>
2021-03-27 19:24:13 +0200

    current location

--- itec-frontend/src/Components/Map.tsx
+++ itec-frontend/src/Components/Map.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import * as React from "react";
+import { LocationModel } from "../Models/Location";
+import { getUserLocation } from "../Services/Location";
 
 declare const window: any;
 
@@ -6,23 +8,28 @@ export const Map = () => {
   // Create a reference to the HTML element we want to put the map on
   const mapRef = React.useRef(null);
 
+  const [location, setLocation] = React.useState<LocationModel>(
+    getUserLocation()
+  );
+
   /**
    * Create the map instance
    * While `useEffect` could also be used here, `useLayoutEffect` will render
    * the map sooner
    */
+
   React.useLayoutEffect(() => {
     // `mapRef.current` will be `undefined` when this hook first runs; edge case that
     if (!mapRef.current) return;
     const H = window.H;
     const platform = new H.service.Platform({
-        apikey: "0m3SljGdGlOsmb-s1OajUHK9-IhMaiML60DIZfxiXhw"
+      apikey: "0m3SljGdGlOsmb-s1OajUHK9-IhMaiML60DIZfxiXhw",
     });
     const defaultLayers = platform.createDefaultLayers();
     const hMap = new H.Map(mapRef.current, defaultLayers.vector.normal.map, {
-      center: { lat: 50, lng: 5 },
-      zoom: 5,
-      pixelRatio: window.devicePixelRatio || 1
+      center: location,
+      zoom: 18,
+      pixelRatio: window.devicePixelRatio || 1,
     });
 
     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hMap));
@@ -34,11 +41,11 @@ export const Map = () => {
     return () => {
       hMap.dispose();
     };
-  }, [mapRef]); // This will run this hook every time this ref is updated
+  }, [mapRef, location]); // This will run this hook every time this ref is updated
 
   return (
-      <div className="map">
-        <div ref={mapRef} style={{ height: "100vh", width:"100vw"}} />;
-      </div>
-  )
+    <div className="map">
+      <div ref={mapRef} style={{ height: "100vh", width: "100vw" }} />;
+    </div>
+  );
 };
