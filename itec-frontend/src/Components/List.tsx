9b1d29bbdc37db12d34207dafdf963ba0e91ecd1
Adelin.Chis <Email>
2021-03-27 19:43:55 +0200

    fix production build

--- itec-frontend/src/Components/List.tsx
+++ itec-frontend/src/Components/List.tsx
@@ -1,71 +1,74 @@
-import { Button, createStyles, Grid, makeStyles, TextField, Theme } from '@material-ui/core'
-import React, { useState } from 'react'
-import SearchIcon from '@material-ui/icons/Search';
-import DateFnsUtils from '@date-io/date-fns';
 import {
-  MuiPickersUtilsProvider,
-  KeyboardTimePicker,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
+  Button,
+  createStyles,
+  makeStyles,
+  TextField,
+  Theme,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     container: {
-      display: 'flex',
-      flexWrap: 'wrap',
+      display: "flex",
+      flexWrap: "wrap",
     },
     textField: {
       marginLeft: theme.spacing(1),
       marginRight: theme.spacing(1),
       width: 200,
     },
-  }),
+  })
 );
 
 export default function List() {
-    const classes = useStyles();
-    const [selectedDate, setSelectedDate] = React.useState<Date | null>(
-        new Date('2014-08-18T21:11:54'),
-      );
+  const classes = useStyles();
+  const [selectedDate, setSelectedDate] = React.useState<Date | null>(
+    new Date()
+  );
 
-    const handleDateChange = (date: Date | null) => {
-        setSelectedDate(date);
-      };
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
 
-    return (
-        <div className="list">
-            <form className={classes.container} noValidate>
-                <div className="fields">
-                    <TextField 
-                        label="Country"
-                        id="outlined-size-small"
-                        defaultValue=""
-                        variant="outlined"
-                        size="small"
-                    />
-                    <TextField
-                        id="date"
-                        label="Arrival"
-                        type="date"
-                        defaultValue="2017-05-24"
-                        className={classes.textField}
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                    />
-                    <TextField
-                        id="date"
-                        label="Departure"
-                        type="date"
-                        defaultValue="2017-05-24"
-                        className={classes.textField}
-                        InputLabelProps={{
-                        shrink: true,
-                        }}
-                    />
-                    <Button><SearchIcon/>Search</Button>
-                </div>
-            </form>
+  return (
+    <div className="list">
+      <form className={classes.container} noValidate>
+        <div className="fields">
+          <TextField
+            label="Country"
+            id="outlined-size-small"
+            defaultValue=""
+            variant="outlined"
+            size="small"
+          />
+          <TextField
+            id="date"
+            label="Arrival"
+            type="date"
+            defaultValue="2017-05-24"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <TextField
+            id="date"
+            label="Departure"
+            type="date"
+            defaultValue="2017-05-24"
+            className={classes.textField}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Button>
+            <SearchIcon />
+            Search
+          </Button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
