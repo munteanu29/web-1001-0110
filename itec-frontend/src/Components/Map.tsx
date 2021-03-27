aae113fe54e55f1ccb117021ed75bee02f625c38
Denisia <denisia.paraiala@student.upt.ro>
2021-03-27 19:24:57 +0200

    add data picker

--- itec-frontend/src/Components/Map.tsx
+++ itec-frontend/src/Components/Map.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
 
 declare const window: any;
+const H = window.H;
+
+var dataPoints: any[] = [];
+dataPoints.push(new H.clustering.DataPoint(57.01, 1.01));
+dataPoints.push(new H.clustering.DataPoint(33.01, 12.01));
+dataPoints.push(new H.clustering.DataPoint(40.01, 3.01));
 
 export const Map = () => {
   // Create a reference to the HTML element we want to put the map on
@@ -14,7 +20,6 @@ export const Map = () => {
   React.useLayoutEffect(() => {
     // `mapRef.current` will be `undefined` when this hook first runs; edge case that
     if (!mapRef.current) return;
-    const H = window.H;
     const platform = new H.service.Platform({
         apikey: "0m3SljGdGlOsmb-s1OajUHK9-IhMaiML60DIZfxiXhw"
     });
@@ -25,9 +30,22 @@ export const Map = () => {
       pixelRatio: window.devicePixelRatio || 1
     });
 
+    var clusteredDataProvider = new H.clustering.Provider(dataPoints);
+
+    // Create a layer that includes the data provider and its data points: 
+    var layer = new H.map.layer.ObjectLayer(clusteredDataProvider);
+
     var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(hMap));
 
+    // var noiseMarker = new H.map.Marker(57.01, 1.01);
+    // noiseMarker.setData(57.01, 1.01);
+
     const ui = H.ui.UI.createDefault(hMap, defaultLayers);
+
+    hMap.addLayer(layer);
+    //hMap.addLayer(noiseMarker);
+
+    //hMap.addObject(new H.map.Marker.setData(57.01, 1.01));
 
     // This will act as a cleanup to run once this hook runs again.
     // This includes when the component un-mounts
